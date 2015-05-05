1fc1491ea6e9b490c9958947fbfc703561c313b0
yukoga <yukoga@gmail.com>
2015-05-06 04:08:20 +0900

    create release version.

--- example/sample1.js
+++ example/sample1.js
@@ -3,11 +3,11 @@ var modelmapper = require('../'),
     ;
 
 var usermodel = modelmapper.load('./example/models/user.js');
-console.log(usermodel);
+console.log(usermodel.get('user')[0].name);
 
-// var models2 = modelmapper.load(['./example/models/user.js', './example/models/article.js']);
-// console.log(models2);
+var models2 = modelmapper.load(['./example/models/user.js', './example/models/article.js']);
+console.log(models2.get('article')[1].title);
 
-// var models = modelmapper.load('./example/models/*.js');
-// console.log(models);
+var models = modelmapper.load('./example/models/*.js');
+console.log(models.get('article')[0].index.chap1);
 
